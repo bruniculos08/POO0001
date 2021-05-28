a484e7dacc1973ed149b929208e6ad4dcb82ca53
Bruno Rafael dos Santos <bruniculos2014@gmail.com>
2021-05-27 23:15:40 -0300

    update

--- AulaPOO/src/App.java
+++ AulaPOO/src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int x = 10;
+        String nome;
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Hello, World!" + 10 + x + nome); //não há funções em linguagem orientada a objeto
+                                             //apenas objetos executam ações; 
+                                             //nesse caso o objeto System realiza a ação println
+                                             //essa ação é realizada através da saída padrão out (tela)
     }
 }
