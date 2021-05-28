41018e099be66ddd4bf02890a4fdbfda409a712e
Bruno Rafael dos Santos <bruniculos2014@gmail.com>
2021-05-27 23:43:59 -0300

    update

--- AulaPOO/src/App.java
+++ AulaPOO/src/App.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        int x = 10;
         String nome;
-        Scanner scan = new Scanner (System.in);
-        System.out.println("Hello, World!" + 10 + x + nome); //não há funções em linguagem orientada a objeto
+        int x = 10;
+        System.out.println("Print Your Name: "); // vs code ou intellij
+        Scanner scan = new Scanner(System.in);
+        nome = scan.next();
+        System.out.println("Hello, World " + nome); //não há funções em linguagem orientada a objeto
                                              //apenas objetos executam ações; 
                                              //nesse caso o objeto System realiza a ação println
                                              //essa ação é realizada através da saída padrão out (tela)
