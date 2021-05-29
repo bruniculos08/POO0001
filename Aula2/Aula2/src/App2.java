fa41f150c20fdb1b9a4d214af01d33d0779e7e35
Bruno Rafael dos Santos <bruniculos2014@gmail.com>
2021-05-28 22:52:50 -0300

    Update App2.java

--- Aula2/Aula2/src/App2.java
+++ Aula2/Aula2/src/App2.java
@@ -1,12 +1,13 @@
 public class App2 {
-    public static void main(String args[]){     // String args[] é um vetor de strings em java
-        Quadrado q = new Quadrado(0, 1, 10);            // Método Construtor
-                                                // a variável "q" irá apontar para um novo objeto de classe Quadrado()
-                                                // quando o tipo de dado começa com letra maiscúla significa que é uma classe e logo um objeto será criado
-                                                // um objeto possui operações e atributos
+    public static void main(String args[]){                 // String args[] é um vetor de strings em java
+        // Chamada do método construtor
+        Quadrado q = new Quadrado(0, 1, 10);
+                                                         // a variável "q" irá apontar para um novo objeto de classe Quadrado()
+                                                         // quando o tipo de dado começa com letra maiscúla significa que é uma classe e logo um objeto será criado
+                                                         // um objeto possui operações e atributos
         int area;
-        //q.x = 0;
-        //q.y = 0;
+        //q.x = 0; caso o atributo x do quadrato tivesse em acesso public poderiámos alterar o atributo desta maneira
+        //q.y = 0; porém como o acesso é private, caso tentássemos isso ocorreria um erro
         //q.lado = 10;
 
         q.calcularArea();
