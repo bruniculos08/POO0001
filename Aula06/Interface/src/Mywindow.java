3ba5f29e2ff22b33d86d783965dd2f95d32876da
Bruno Rafael dos Santos <bruniculos2014@gmail.com>
2021-07-02 22:32:49 -0300

    update

--- Aula06/Interface/src/Mywindow.java
+++ Aula06/Interface/src/Mywindow.java
@@ -1,9 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Mywindow extends JFrame{
+public class Mywindow extends JFrame implements ActionListener{ //^ implementar uma interface, pois esta não tem código, o código tem que ser escrito antes da implementação
+    JLabel label = new JLabel("This is a label"); //^ Objeto Label (uma região com um texto ou algo do tipo)
+    JButton button1 = new JButton("Okay");        //^ Objeto Button (botão escrito "Okay")
+    JButton button2 = new JButton("Hey"); 
+
     public Mywindow(){
-        super("the title of my window is it this");
+        super("The title of my window is it this");
         startComponents();                              //^ método para criar os componentes da janela
         //setSize(1100, 1100);                          //^ Define o tamanho da janela
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //^ A janela fecha ao encerrar o código
@@ -11,12 +18,30 @@ public class Mywindow extends JFrame{
     }
     
     private void startComponents(){
-        JLabel label = new JLabel("This is a label"); //^ Objeto Label (uma região com um texto ou algo do tipo)
-        JButton button = new JButton("Okay");         //^ Objeto Button (botão escrito "Okay")
-        button.setBackground(Color.BLUE);             //^ Altera a cor de fundo do botão para azul
+      //JLabel label = new JLabel("This is a label"); //^ Objeto Label (uma região com um texto ou algo do tipo)
+      //JButton button = new JButton("Okay");         //^ Objeto Button (botão escrito "Okay")
+        button1.setBackground(Color.WHITE);           //^ Altera a cor de fundo do botão para azul
+        button2.setBackground(Color.WHITE);
+
+        button1.actionPerformed("Okay");
+        button2.actionPerformed("Hey");
 
         setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS)); //^ Organiza o layout sobre o eixo Y (fila vertical)
-        add(label, BorderLayout.NORTH);                               //^ Coloca o botão no norte do Layout Border se este for o Layout
-        add(button, BorderLayout.SOUTH);                              //^ Coloca o botão no sul do Layout Border se este for o Layout
+        add(label);//, BorderLayout.NORTH);                           //^ Coloca o botão no norte do Layout Border se este for o Layout
+        add(button1);//, BorderLayout.SOUTH);                          //^ Coloca o botão no sul do Layout Border se este for o Layout
+        add(button2);//, BorderLayout.SOUTH); 
+
+        //& Listeners ("Observadores")
+        button1.addActionListener(this);
     }
+
+    public void actionPerformed(ActionEvent e){
+        if (e == button1) {
+            System.out.println("Button was pressed!");
+        }
+        if (e == button2) {
+            System.out.println("Hello");
+        }
+    }
+
 }
