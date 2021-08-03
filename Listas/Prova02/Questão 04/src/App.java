2e8eb98f83b9a747013cf433ec294cfeb11dc4da
Bruno Rafael dos Santos <bruniculos2014@gmail.com>
2021-08-03 20:01:05 -0300

    update

---
+++ Listas/Prova02/Questão 04/src/App.java
@@ -0,0 +1,10 @@
+import javax.swing.*; //* asterisco importa tudo de uma biblioteca
+import java.awt.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Teste inciando");
+        new Janela();
+        System.out.println("Teste terminando");
+    }
+}
---
+++ Listas/Prova02/Questão 04/src/ContadorListener.java
@@ -0,0 +1,5 @@
+public class ContadorListener implements Listener {
+    void oValorMudou(int novoValor){
+        System.out.println("Ocorreu alteração no contador para: " + novoValor);
+    }
+}
