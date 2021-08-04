303eac6884401df8d1220f791084cc7387b3b8d3
Bruno Rafael dos Santos <bruniculos2014@gmail.com>
2021-08-03 21:50:49 -0300

    update

--- Listas/Prova02/Questão 04/src/ContadorListener.java
+++ Listas/Prova02/Questão 04/src/ContadorListener.java
@@ -2,7 +2,7 @@ public class ContadorListener implements Listener {
     public int valor;
     @Override
     public void oValorMudou(int novoValor){
-        System.out.println("Ocorreu alteração no contador para: " + novoValor);
+        System.out.println("Ocorreu alteracao no contador para: " + novoValor);
         valor = novoValor;
     }
 }
