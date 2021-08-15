07187ec883d66f76880823c051acb732e86f385c
Bruno Rafael dos Santos <bruniculos2014@gmail.com>
2021-08-15 15:08:46 -0300

    update

--- Listas/Trabalho01/src/views/TelaInicial.java
+++ Listas/Trabalho01/src/views/TelaInicial.java
@@ -20,15 +20,22 @@ public class TelaInicial extends JFrame {
     
     private void setSettings() {
         setSize(500,500);
-        setVisible(true);
+        setLayout(new BorderLayout());
+        setTextArea();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
     }
 
     private void setTextArea(){
-        JTextArea textArea = new JTextArea(5, 20);
+        JTextArea textArea = new JTextArea("This is a text area: ");
         textArea.setCaretPosition(textArea.getDocument().getLength());
+        textArea.setRows(20);
+        textArea.setColumns(50);
+        textArea.setEditable(true);
+        getContentPane().add(new JScrollPane(textArea), BorderLayout.CENTER);
+        //JScrollPane scrollPane = new JScrollPane(textArea);
+        //scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        //scrollPane.setPreferredSize(new Dimension(250, 250));
+        add(textArea);
     }
-
-
-
 }
