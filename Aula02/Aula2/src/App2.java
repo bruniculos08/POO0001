1f5719b7cd9e1678586d686b9797313c776194fa
Bruno Rafael dos Santos <bruniculos2014@gmail.com>
2021-10-14 17:28:35 -0300

    update

--- Aula02/Aula2/src/App2.java
+++ Aula02/Aula2/src/App2.java
@@ -5,7 +5,7 @@ public class App2 {
         // Chamada do método construtor de Objeto da Classe Quadrado
         Quadrado q = new Quadrado(0, 1, 10);
         // Chamando método construtor de Objeto da Classe Retângulo
-        Quadrado r = new Retangulo(0, 0, 8, 10);
+        Retangulo r = new Retangulo(0, 0, 8, 10);
 
         // A variável "q" irá apontar para um novo objeto de classe Quadrado()
         // Quando o tipo de dado começa com letra maiscúla significa que é uma classe e logo um objeto (um objeto possui operações e atributos) será criado
